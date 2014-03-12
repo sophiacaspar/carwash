56cb89ba2b02b043fc0309cf58b06f58ce501139
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:31:13 +0100

    Update Simulator.java

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,13 +1,28 @@
 package simulator;
 
+/**
+ * This class simulates the car wash. The
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ * 
+ */
 public class Simulator {
 
 	private EventQueue eventQueue;
 
+	/**
+	 * The constructor.
+	 * 
+	 * @param eventQueue
+	 *            Creates the queue.
+	 */
 	public Simulator(EventQueue eventQueue) {
 		setEventQueue(eventQueue);
 	}
 
+	/**
+	 * Stars the queue.
+	 */
 	public void run() {
 		do {
 			Event event = eventQueue.first();
@@ -17,9 +32,13 @@ public class Simulator {
 		} while (!eventQueue.isEmpty());
 	}
 
+	/**
+	 * 
+	 * @param eventQueue
+	 *            Initiates the queue.
+	 */
 	public void setEventQueue(EventQueue eventQueue) {
 		this.eventQueue = eventQueue;
 	}
 
 }
-
