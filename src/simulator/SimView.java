8a98d9e2a63ddca68790e039251f97b1aa1f1e9c
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:59:51 +0100

    Update SimView.java

--- src/simulator/SimView.java
+++ src/simulator/SimView.java
@@ -3,6 +3,12 @@ package simulator;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * Keeps track update classes.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ *
+ */
 public abstract class SimView implements Observer {
 
 	@Override
