d744a6325cfda0b1fee473592690ab9cde42cf4b
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:52:44 +0100

    Update EventQueue.java

--- src/simulator/EventQueue.java
+++ src/simulator/EventQueue.java
@@ -8,21 +8,36 @@ import java.util.NoSuchElementException;
  * This class adds and removes event from the queue. The queue.
  * 
  * @author emmacarlsson, sophiacaspar, malinross
- *
+ * 
  */
 public class EventQueue {
 
 	private ArrayList<Event> eventQueue = new ArrayList<Event>();
 
+	/**
+	 * Event is added and sorted.
+	 * 
+	 * @param event
+	 *            The event.
+	 */
 	public void add(Event event) {
 		eventQueue.add(event);
-		Collections.sort(eventQueue); //Sorts the queue.
+		Collections.sort(eventQueue); // Sorts the queue.
 	}
 
+	/**
+	 * Clears the queue.
+	 */
 	public void clear() {
 		eventQueue.clear();
 	}
 
+	/**
+	 * Returns the first event in the queue.
+	 * 
+	 * @return first event.
+	 * @throws NoSuchElementException
+	 */
 	public Event first() throws NoSuchElementException {
 		if (eventQueue.isEmpty()) {
 			throw new NoSuchElementException();
@@ -31,10 +46,20 @@ public class EventQueue {
 		}
 	}
 
+	/**
+	 * If the queue is empty.
+	 * 
+	 * @return true or false
+	 */
 	public boolean isEmpty() {
 		return eventQueue.isEmpty();
 	}
 
+	/**
+	 * Removes the first event in the queue.
+	 * 
+	 * @throws NoSuchElementException
+	 */
 	public void removeFirst() throws NoSuchElementException {
 		if (eventQueue.isEmpty()) {
 			throw new NoSuchElementException();
@@ -43,6 +68,11 @@ public class EventQueue {
 		}
 	}
 
+	/**
+	 * The queue size.
+	 * 
+	 * @return the size.
+	 */
 	public int size() {
 		return eventQueue.size();
 	}
