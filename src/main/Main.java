f218f68de800fef9c00c18036f2b00e67b978c03
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:24:47 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+/**
+ * This program simulates a car wash.
+ */
+
 import event.Start;
 import event.Stop;
 import simulator.EventQueue;
@@ -7,24 +11,33 @@ import simulator.Simulator;
 import state.CarWashState;
 import view.CarWashView;
 
+/**
+ * Here some variables are set which determine qualities about the car wash.
+ */
 public class Main {
 
 	public static int fastMachines = 2;
-	public static int slowMachines = 2;
+	public static int slowMachines = 4;
 	public static double fastLow = 2.8;
-	public static double fastHigh = 4.6;
-	public static double slowLow = 3.5;
+	public static double fastHigh = 5.6;
+	public static double slowLow = 4.5;
 	public static double slowHigh = 6.7;
-	public static double lambda = 2.0;
+	public static double lambda = 1.5;
 	public static long seed = 1234;
-	public static int queueSize = 5;
+	public static int queueSize = 7;
 	public static int stopTime = 15;
-	
+
+	/**
+	 * Creates a new eventQueue, creates a new cwState and starts the simulation
+	 * which will run until stopTime is reached.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		EventQueue eventQueue = new EventQueue();
-
 		CarWashState cwState = new CarWashState(fastMachines, slowMachines);
+
 		cwState.fastWashTime(fastLow, fastHigh, seed);
 		cwState.slowWashTime(slowLow, slowHigh, seed);
 		cwState.nextArrival(lambda, seed);
