c5241435d38ee3fa32526f487e2bb28abe6d040c
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:58:00 +0100

    Update SimState.java

--- src/simulator/SimState.java
+++ src/simulator/SimState.java
@@ -2,6 +2,12 @@ package simulator;
 
 import java.util.Observable;
 
+/**
+ * Creates observable variables.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinroos
+ *
+ */
 public abstract class SimState extends Observable {
 
 	public double currentTime;
