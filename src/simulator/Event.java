3e5f990cd9c09877998a8f4f62cb5e5e8cd025ed
emakk <emmcar-2@student.ltu.se>
2014-03-12 15:41:12 +0100

    Update Event.java

--- src/simulator/Event.java
+++ src/simulator/Event.java
@@ -1,11 +1,20 @@
 package simulator;
 
-
+/**
+ * Determines what the subclasses must include and sorts the queue depending on
+ * the time.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ * 
+ */
 public abstract class Event implements Comparable<Event> {
 
 	public double startTime;
 
 	@Override
+	/**
+	 * Sorting the queue depending on the time.
+	 */
 	public int compareTo(Event otherEvent) {
 		if (this.startTime == otherEvent.startTime) {
 			return 0;
@@ -15,7 +24,7 @@ public abstract class Event implements Comparable<Event> {
 			return 1;
 		}
 	}
-
+	
 	public abstract void execute();
 
 	@Override
