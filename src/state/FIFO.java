d3398c753de30be96c63b6ce50c5b8d9e622a91c
emakk <emmcar-2@student.ltu.se>
2014-03-17 13:46:38 +0100

    Update FIFO.java

--- src/state/FIFO.java
+++ src/state/FIFO.java
@@ -3,13 +3,31 @@ package state;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
+/**
+ * Defines the car queue.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ * 
+ */
 public class FIFO {
-	private ArrayList<Car> queue = new ArrayList<Car>();
+	private ArrayList<Car> queue = new ArrayList<Car>(); // Creates a list for
+															// the cars.
 
+	/**
+	 * Adds the car.
+	 * 
+	 * @param car
+	 */
 	public void add(Car car) {
 		queue.add(car);
 	}
 
+	/**
+	 * Returns the first car in the queue.
+	 * 
+	 * @return the first car.
+	 * @throws NoSuchElementException
+	 */
 	public Car first() throws NoSuchElementException {
 		if (queue.isEmpty()) {
 			throw new NoSuchElementException();
@@ -18,10 +36,20 @@ public class FIFO {
 		}
 	}
 
+	/**
+	 * If the queue is empty it returns true or false.
+	 * 
+	 * @return true or false if the queue is empty.
+	 */
 	public boolean isEmpty() {
 		return queue.isEmpty();
 	}
 
+	/**
+	 * Removes the first car in the queue.
+	 * 
+	 * @throws NoSuchElementException
+	 */
 	public void removeFirst() throws NoSuchElementException {
 		if (queue.isEmpty()) {
 			throw new NoSuchElementException();
@@ -30,6 +58,10 @@ public class FIFO {
 		}
 	}
 
+	/**
+	 * 
+	 * @return the queue size.
+	 */
 	public int size() {
 		return queue.size();
 	}
