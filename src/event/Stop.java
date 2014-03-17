6037e9ce64ff90dd135241070995325224d98e0e
emakk <emmcar-2@student.ltu.se>
2014-03-17 13:44:30 +0100

    Update Stop.java

--- src/event/Stop.java
+++ src/event/Stop.java
@@ -4,11 +4,27 @@ import simulator.Event;
 import simulator.EventQueue;
 import state.CarWashState;
 
+/**
+ * Defines a stop event and stops the process.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ * 
+ */
 public class Stop extends Event {
 
 	CarWashState state;
 	EventQueue eventQueue;
 
+	/**
+	 * The constructor. Defines the stop event.
+	 * 
+	 * @param stopTime
+	 *            Saves the stop time to Event.
+	 * @param state
+	 *            Saves the state.
+	 * @param eventQueue
+	 *            saves the queue.
+	 */
 	public Stop(double stopTime, CarWashState state, EventQueue eventQueue) {
 		super.startTime = stopTime;
 		this.state = state;
@@ -16,6 +32,13 @@ public class Stop extends Event {
 	}
 
 	@Override
+	/**
+	 * Determines the state, the Idle-, and Queue time and sets the start time for the stop event.
+	 * 
+	 * Updates the observers.
+	 * 
+	 * Clears the event queue.
+	 */
 	public void execute() {
 		state.setCurrentEvent(this);
 		state.setTime(startTime);
@@ -29,6 +52,9 @@ public class Stop extends Event {
 	}
 
 	@Override
+	/**
+	 * Returns the name of the event in string form.
+	 */
 	public String toString() {
 		return "Stop";
 	}
