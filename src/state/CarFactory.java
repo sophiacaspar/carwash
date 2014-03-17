27a1db7a282a6a71ae3ff745aee530dd266b94fa
emakk <emmcar-2@student.ltu.se>
2014-03-17 13:45:36 +0100

    Update CarFactory.java

--- src/state/CarFactory.java
+++ src/state/CarFactory.java
@@ -1,8 +1,18 @@
 package state;
 
+/**
+ * Creates the car.
+ * 
+ * @author emmacarlsson, sophiacaspar, malinross
+ *
+ */
 public class CarFactory {
 	private int id = 0;
 
+	/**
+	 * Returns the new car with an ID.
+	 * @return new id
+	 */
 	public Car makeCar() {
 		return new Car(id++);
 	}
